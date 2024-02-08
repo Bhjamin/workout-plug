f9a8cc2b02c6e3a715a9d4af86b32e2fafd794d0
Ben Hancock <benjamin@hancock1.com>
2024-02-07 21:01:00 -0700

    Added framer motion animation

--- src/app/components/Day.tsx
+++ src/app/components/Day.tsx
@@ -1,5 +1,7 @@
+import { AnimatePresence, animate } from "framer-motion";
+
 const Day = () => {
-  return <div className="w-[14.2857142857%]"></div>;
+  return <div className="w-full h-full bg-white border-4 border-black"></div>;
 };
 
 export default Day;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import Month from "./components/Month";
 
 const Homepage = () => {
   const currentDate = new Date();
-  console.log(currentDate.getDay());
 
   return (
     <div className="w-full h-screen flex flex-col items-center justify-start bg-gradient-to-br from-[#44355B] to-[#EE5622]">
@@ -15,8 +14,11 @@ const Homepage = () => {
           src="https://camo.githubusercontent.com/3bcd317876dc122d3055613c7f5450134050d0c5a8683807c6f2e8e2178737b0/68747470733a2f2f6672616d657275736572636f6e74656e742e636f6d2f696d616765732f34386861395a52396f5a51475136675a38595566456c50335430412e706e67"
         />
       </div>
-      <div className="w-full h-full flex justify-center items-center px-2 xs:px-4 sm:px-8 md:px-12 lg:px-16 xl:px-20 2xl:px-24">
-        <Month />
+      <div className="w-full h-full flex justify-center items-center">
+        <Month
+          year={currentDate.getFullYear()}
+          month={currentDate.getMonth()}
+        />
       </div>
     </div>
   );
